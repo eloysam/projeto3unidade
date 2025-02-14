95773b31c8ee935b643b330b1318b10268f2d4c9
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2025-02-14 14:23:06 -0300

    .

--- projeto3unidade/src/main/java/com/example/ifbademo/controller/ConsultaFornecedorBean.java
+++ projeto3unidade/src/main/java/com/example/ifbademo/controller/ConsultaFornecedorBean.java
@@ -1,10 +1,31 @@
 package com.example.ifbademo.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import com.example.ifbademo.model.Fornecedor;
+import com.example.ifbademo.model.Produto;
+import com.example.ifbademo.service.FornecedorService;
+
+import lombok.Getter;
 
 @Component
 @Scope("view")
 public class ConsultaFornecedorBean {
     
+    @Getter
+    private Fornecedor fornecedor;
+    @Getter
+    private FornecedorService forService;
+    @Getter
+    private Produto produto;
+
+    @PostConstruct
+    public void init(){
+        fornecedor = forService.buscarTodos();
+    }
+
+
 }
